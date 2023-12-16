68c399ee641eb4e5d97415dab5ed7644f8bf14e4
Davi Gledson <davigledson@outlook.com>
2023-12-16 06:59:39 -0300

    classe de upload importada

--- conexao.php
+++ conexao.php
@@ -1,4 +1,10 @@
 <?php 
+namespace conexao;
+use mysqli;
+use Verot\Upload\Upload;
+
+require_once 'vendor/autoload.php';
+
 $localhost = 'localhost';
 $usuario = 'root';
 $senha = '';
@@ -10,6 +16,20 @@ $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 var_dump($dados);
 var_dump($_FILES['arquivo']);
+
+
+if(!empty($_FILES['arquivo'])){
+    $upload = (new Upload($_FILES['arquivo']));
+if($upload->uploaded){
+    $upload->image_resize = true;
+    $upload->image_x = 400;
+    $upload->image_y = 400;
+    $upload->process('imgs/');
+}
+
+
+}
+
 // function salvar(){
 //     
 //     $query ="INSERT INTO fotos (`:nome`, `caminho`)  VALUES (:nome, caminho)" ;
